38ef84c893212e6ca075347d70ef1019fceaa5b6
Pedro Monteiro <91344309+montteiropedro@users.noreply.github.com>
2026-08-17 09:29:47 -0300

    fix(sidekiq): move UI mount point out of the API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      mount Sidekiq::Web => "/sidekiq"
 
       post "library/scan", to: "library#scan"
 
