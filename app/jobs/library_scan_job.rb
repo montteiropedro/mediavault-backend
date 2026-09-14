dd516e3f41bde77756e28bc64e078d4801bd21ca
Pedro Monteiro <91344309+montteiropedro@users.noreply.github.com>
2026-09-14 16:44:52 -0300

    feat: updates library scan job and related services to use the job progress tracker

--- app/jobs/library_scan_job.rb
+++ app/jobs/library_scan_job.rb
@@ -2,7 +2,21 @@ class LibraryScanJob < ApplicationJob
   queue_as :default
 
   def perform
-    ScanMoviesService.call
-    ScanShowsService.call
+    progress = JobProgress.new(job_id)
+
+    new_movies = progress.run("movies", weight: 10) { |progress| ScanMoviesService.call(progress:) }
+    new_episodes = progress.run("shows", weight: 10) { |progress| ScanShowsService.call(progress:) }
+
+    new_playables = new_movies + new_episodes
+
+    if new_playables.empty?
+      progress.complete!
+    else
+      progress.run_async("metadata", weight: 80, total: new_playables.size)
+      new_playables.each { |playable| Playable::MetadataProcessingJob.perform_later(playable, parent_job_id: job_id) }
+    end
+  rescue => e
+    progress&.fail!(e.message)
+    raise
   end
 end
--- app/jobs/playable/metadata_processing_job.rb
+++ app/jobs/playable/metadata_processing_job.rb
@@ -1,12 +1,16 @@
 class Playable::MetadataProcessingJob < ApplicationJob
   queue_as :default
 
-  def perform(media)
+  def perform(media, parent_job_id: nil)
     unless media.is_a?(Movie) || media.is_a?(Episode)
       ApplicationLogger.warn("Unsupported playable type", location: media.class.name)
       return
     end
 
     Playable::MetadataService.call(media)
+  ensure
+    return unless parent_job_id
+
+    JobProgress.async_item_completed!(parent_job_id, "#{media.class.name}:#{media.id}")
   end
 end
